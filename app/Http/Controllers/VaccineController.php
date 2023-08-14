253e23594ed0ccea5b83e2ad58850f5beb3e0f1b
JelloMangune <mangune.jello@auf.edu.ph>
2023-08-15 02:02:27 +0800

    Modified Vaccine Update Status Validation

--- app/Http/Controllers/VaccineController.php
+++ app/Http/Controllers/VaccineController.php
@@ -15,7 +15,7 @@ class VaccineController extends Controller
      */
     public function index()
     {
-        $vaccines = Vaccine::all();
+        $vaccines = Vaccine::whereIn('status', [0, 1])->get();
         return response()->json(['data' => $vaccines], 200);
     }
 
@@ -36,7 +36,28 @@ class VaccineController extends Controller
         return response()->json(['data' => $vaccine], 200);
     }
 
-    // ... (Other methods)
+    public function store(Request $request)
+    {
+        // Check if the authenticated user has admin privileges (user_type = 0)
+        if (Auth::user()->user_type !== 0) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        // Validate the request data
+        $request->validate([
+            'name' => 'required|string',
+            'short_name' => 'required|string',
+        ]);
+
+        // Create the new vaccine
+        $vaccine = new Vaccine();
+        $vaccine->name = $request->input('name');
+        $vaccine->short_name = $request->input('short_name');
+        $vaccine->status = 1; // Assuming the default status is Active
+        $vaccine->save();
+
+        return response()->json(['data' => ['message' => 'Vaccine added successfully', 'id' => $vaccine->id]], 201);
+    }
 
     /**
      * Delete a vaccine by ID (admin only).
